27898de72f276724e7bb720d597effcad663d53f
Geoff Simons <geoff.simons@gmail.com>
2016-12-26 22:06:10 -0800

    Validation errors now return 400

--- lab-geoff/lib/error-middleware.js
+++ lab-geoff/lib/error-middleware.js
@@ -12,6 +12,12 @@ module.exports = function(err, req, res, next) {
     return next();
   }
 
+  if(err.name == 'ValidationError') {
+    err = createError(400, err.message);
+    res.status(err.status).send(err.name);
+    return next();
+  }
+
   debug('server error:',err.name);
   err = createError(500, err.message);
   res.status(err.status).send(err.name);
